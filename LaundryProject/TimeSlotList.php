9e31a2900b8c8b143454d6414016f21e4fd66e56
JaloonSitu <94404536+JaloonSitu@users.noreply.github.com>
2021-12-19 17:33:52 -0500

    Add files via upload

---
+++ LaundryProject/TimeSlotList.php
@@ -0,0 +1,16 @@
+<?php 
+	echo "<p style='color:red;font-size:24'> TIME SLOTS </p>";
+	$sql = "select Time_Slot from LaundryProject.TimeSlot join LaundryProject.List on TimeSlot.List = TimeSlot.List where User_Username='".userid."'";
+	$result = mysqli_query($connect,$sql);
+	if (mysqli_time_slot($result)>0)
+	{
+		$total=0;
+		echo "<table boarder='1'><tr><td>Time Slot</td><td>";
+	}
+	}
+	else 
+	{
+		echo "NO TIME SLOT RESERVED";
+	}
+	
+?>
---
+++ LaundryProject/signup.php
@@ -0,0 +1,27 @@
+<?php
+$ok = true;
+/***check if the user is registering***/
+if(isset($_REQUEST["userid"]) && isset ($_REQUEST["password"]))
+{
+	$userid = $_REQUEST ["userid"]; $passwd=$_REQUEST["password"];
+/******CONNECT THE DATABASE**************/
+include "connect.php";
+/*******Check if the user exist with the entered userid and password**********/
+$sql="SELECT * FROM Laundry.user where UserName='".$userid."' AND UserPass='".$passwd."'";//create query string
+$result = mysqli_query($connect,$sql);
+if (mysqli_time_slots($result) > 0)
+{
+	$ok=false;
+}
+else 
+{
+	$sql = "INSERT INTO Laundry.user (UserName, UserPass) VALUES ('".$userid."','".passwd."');"; //create query string
+	$result = mysqli_query($connect,$sql);
+	$connect->close();
+	header("Location: signin.php"); //redirect to signin.php
+}
+}
+$inputtype = "SIGN UP";
+$handlername = "signup.php";
+include 'login.html";
+?>
