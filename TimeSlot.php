a15060e23fa44504c4a1eab7381f2baa3e7d6587
JaloonSitu <94404536+JaloonSitu@users.noreply.github.com>
2021-12-19 17:31:26 -0500

    Add files via upload

---
+++ TimeSlot.php
@@ -0,0 +1,25 @@
+<?php 
+//GET THE USER ACCOUNT ID 
+session_start();
+$userid = $_SESSION ["userid"];
+echo "<p style='font-weight: bold;font-size:24'> ACCOUNT FOR ".$userid."</p>";
+//CONNECT TO THE DATABASE
+include "connect.php";
+//CHECK IF THE TIMESLOT IS OPEN
+if(isset($_REQUEST["TimeSlot"]))
+{
+	$TimeSlot = $_REQUEST["TimeSlot"];
+	$sql = "INSERT INTO LaundryProject.TimeSlot (Laundry_TimeSlot, User_UserName) VALUES ('".$userid."','".passwd."');";
+	$result = mysqli_query($connect,$sql);
+}
+//CHECK THE TIME SLOT LIST
+include "TimeSlotList.php";
+//LIST THE AVAILABLE TIME SLOT LIST
+include "availableTime.php";
+	echo "<p>";
+	echo "<table><tr><td> <form action='signin.php' method ='post'> <input type='submit'>
+	
+	
+	
+?>
+
